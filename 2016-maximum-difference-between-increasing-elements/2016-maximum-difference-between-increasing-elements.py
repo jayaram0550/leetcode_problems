1f549272f51a58a2db51b879bd3894f2ee4e342e
Palla Naga Sai Jayaram <150007395+jayaram0550@users.noreply.github.com>
2026-01-07 19:58:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        m=-1
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                m=max(m,nums[j]-nums[i])
+
+        return m
+
+        
