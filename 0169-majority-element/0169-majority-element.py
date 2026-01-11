b635de6b83a899839605956604913a493b38b6c7
Palla Naga Sai Jayaram <150007395+jayaram0550@users.noreply.github.com>
2026-01-11 20:16:08 +0530

    Time: 16 ms (8.97%), Space: 21.2 MB (6.78%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+        
+        return max(d, key=d.get)
