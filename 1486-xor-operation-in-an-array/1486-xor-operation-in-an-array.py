695de8bf0b264db429ed66a5174d5ee651d3127d
Palla Naga Sai Jayaram <150007395+jayaram0550@users.noreply.github.com>
2026-07-15 11:58:31 +0530

    Time: 0 ms (100%), Space: 19.2 MB (87.62%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        a=0
+        while n>0:
+            a^=start
+            start+=2
+            n-=1
+        return a
+
+        
